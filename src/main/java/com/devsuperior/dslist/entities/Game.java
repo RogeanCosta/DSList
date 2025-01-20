0dbb2ff66941002cc774a14284565202fb629dbb
Rogean Costa <programer.rogean@gmail.com>
2025-01-20 19:01:19 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,24 +21,28 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platform;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platform = platform;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -78,11 +82,11 @@ public class Game {
 	}
 
 	public String getPlataform() {
-		return plataform;
+		return platform;
 	}
 
 	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+		this.platform = plataform;
 	}
 
 	public Double getScore() {
